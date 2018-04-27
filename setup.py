1e82beeddb57349f4bcc6046b229d4828404b54a
Dan Ryan <dan@danryan.co>
2018-04-27 00:59:55 -0400

    update setup command
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class UploadCommand(Command):
 
     def run(self):
         self.status('Building Source distribution…')
-        os.system('{0} setup.py sdist'.format(sys.executable))
+        os.system('{0} setup.py sdist bdist_wheel'.format(sys.executable))
         self.status('Uploading the package to PyPi via Twine…')
         os.system('twine upload dist/*')
         self.status('Pushing git tags…')
