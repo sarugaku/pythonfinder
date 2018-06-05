e63ef4351b3a2757c374cd1b828a1adef87bb15e
Dan Ryan <dan@danryan.co>
2018-06-05 16:16:28 -0400

    Version bump for venv support
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/pythonfinder/__init__.py
+++ src/pythonfinder/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, absolute_import
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = ["Finder", "WindowsFinder", "SystemPath"]
 from .pythonfinder import Finder
