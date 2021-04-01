530d8b85a9ac0c9a7eec1ecc24bdc3112d450753
Frost Ming <mianghong@gmail.com>
2021-04-01 19:07:36 +0800

    Pre-bump version

--- src/pythonfinder/__init__.py
+++ src/pythonfinder/__init__.py
@@ -10,7 +10,7 @@ from .exceptions import InvalidPythonVersion
 from .models import SystemPath, WindowsFinder
 from .pythonfinder import Finder
 
-__version__ = "1.2.6"
+__version__ = "1.2.7dev"
 
 
 logger = logging.getLogger(__name__)
