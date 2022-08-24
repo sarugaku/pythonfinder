e65646c764f809642ba578798981d6a00d0c9009
Oz N Tiram <oz.tiram@gmail.com>
2022-08-24 14:03:43 +0200

    Bump version v1.3.1
     * version 1.3.0 not uploaded

--- src/pythonfinder/__init__.py
+++ src/pythonfinder/__init__.py
@@ -10,7 +10,7 @@ from .exceptions import InvalidPythonVersion
 from .models import SystemPath, WindowsFinder
 from .pythonfinder import Finder
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 
 logger = logging.getLogger(__name__)
